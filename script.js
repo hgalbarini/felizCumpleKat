1f517d04838be197511002585439c0596c811284
Hugo Galbarini <hgalbarini@gmail.com>
2025-02-25 22:28:02 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript code for interactivity (optional)
+document.addEventListener('DOMContentLoaded', function() {
+    // Example: Display an alert message
+    alert('Happy Birthday!');
+});
